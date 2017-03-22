8247000c45f3b0cbbb338b16a35329a6dfbc15d7
coy <coy20150428@gmail.com>
2017-03-22 15:35:49 +0800

    commit 0322

---
+++ symmetric.py
@@ -0,0 +1,19 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        if root == None:
+        	return True
+        else:
+        	return self.isMirror(root.left,root.right)
+    def isMirror(self,p,q):
+    	if p == None and q ==None:
+    			return True
+    	if p == None or q == None:
+    			return False
+    	return p.val == q.val and self.isMirror(p.left,q.right) and self.isMirror(p.right,q.left)
