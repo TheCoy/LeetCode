1e8b8373f0093a6ba36bffa3ec41cb6b5470a787
TheCoy <coy20150428@gmail.com>
2020-08-17 23:11:01 +0800

    feat: 无重复最长子串

--- golang/3.无重复字符的最长子串.go
+++ golang/3.无重复字符的最长子串.go
@@ -41,7 +41,25 @@
 
 // @lc code=start
 func lengthOfLongestSubstring(s string) int {
+	hashMap := make(map[byte]int)
+	right, max, length := -1, 0, len(s)
 
+	for i := 0; i < length; i++ {
+		if i != 0 {
+			delete(hashMap, s[i - 1])
+		}
+
+		for right + 1 < length && hashMap[s[right + 1]] == 0 {
+			right++
+			hashMap[s[right]]++
+		}
+
+		if right - i + 1 > max {
+			max = right - i + 1
+		}
+	}
+
+	return max
 }
 // @lc code=end
 
